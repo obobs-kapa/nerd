5526a762b0bb022dc5ec1cc0715ba1f6b9a556c9
obobs <115431830+obobs-kapa@users.noreply.github.com>
2023-07-16 20:56:33 -0300

    Update script.js

--- alimente/functions/script.js
+++ alimente/functions/script.js
@@ -1,11 +1,4 @@
-// Verificar se o acesso está sendo feito por um dispositivo móvel
-function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-// Exemplo de uso
-if (isMobile()) {
- // Selecionar a imagem
+// Selecionar a imagem
 var image = document.getElementById('food-image');
 
 // Variáveis para rastrear a posição inicial do toque
@@ -46,54 +39,4 @@ function moveImage(event) {
   // Atualizar a posição da imagem
   image.style.left = currentX + 'px';
   image.style.top = currentY + 'px';
-}
-
-} else {
-  var personImage = document.getElementById('person-image');
-  var foodImage = document.getElementById('food-image');
-  var statusText = document.getElementById('status-text');
-  var isHungry = true;
-
-  // Adicionar eventos de arrastar e soltar aos elementos
-  personImage.addEventListener('dragover', allowDrop);
-  personImage.addEventListener('drop', drop);
-
-  // Função de manipulação de permitir soltar
-  function allowDrop(event) {
-    event.preventDefault();
-  }
-
-  // Função de manipulação de soltar
-  function drop(event) {
-    event.preventDefault();
-    var foodId = event.dataTransfer.getData('text/plain');
-    var food = document.getElementById(foodId);
-
-    var rect = personImage.getBoundingClientRect();
-    var personImageX = rect.left;
-    var personImageY = rect.top;
-    var personImageWidth = rect.width;
-    var personImageHeight = rect.height;
-
-    var dropX = event.clientX;
-    var dropY = event.clientY;
-
-    if (
-      dropX >= personImageX &&
-      dropX <= personImageX + personImageWidth &&
-      dropY >= personImageY &&
-      dropY <= personImageY + personImageHeight
-    ) {
-      if (isHungry) {
-        personImage.src = 'erguida.png';
-        statusText.innerHTML = 'A pessoa está alimentada. Obrigado!';
-        food.style.display = 'none';
-        isHungry = false;
-      } else {
-        statusText.innerHTML = 'A pessoa já está alimentada.';
-      }
-    } else {
-      statusText.innerHTML = 'Arraste a comida até a pessoa para alimentá-la.';
-    }
-  }
 }
