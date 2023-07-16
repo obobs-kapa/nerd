8fbde7736e530b157596f8c2ab19bb8f596dcebf
obobs <115431830+obobs-kapa@users.noreply.github.com>
2023-07-16 20:52:47 -0300

    Update script.js

--- alimente/functions/script.js
+++ alimente/functions/script.js
@@ -5,57 +5,49 @@ function isMobile() {
 
 // Exemplo de uso
 if (isMobile()) {
-  var personImage = document.getElementById('person-image');
-  var foodImage = document.getElementById('food-image');
-  var statusText = document.getElementById('status-text');
-  var isHungry = true;
+ // Selecionar a imagem
+var image = document.getElementById('food-image');
 
-  // Adicionar eventos de toque aos elementos
-  personImage.addEventListener('touchstart', touchStart);
-  foodImage.addEventListener('touchstart', touchStart);
-  personImage.addEventListener('touchmove', touchMove);
-  personImage.addEventListener('touchend', touchEnd);
+// Variáveis para rastrear a posição inicial do toque
+var startX = 0;
+var startY = 0;
 
-  // Variáveis para rastrear o estado do toque
-  var touchStartX;
-  var touchStartY;
+// Adicionar eventos de toque para iniciar e parar o movimento
+image.addEventListener('touchstart', startDrag);
+image.addEventListener('touchend', stopDrag);
 
-  // Função de início de toque
-  function touchStart(event) {
-    var touch = event.touches[0];
-    touchStartX = touch.clientX;
-    touchStartY = touch.clientY;
-  }
+function startDrag(event) {
+  // Impedir que outros eventos de toque ocorram
+  event.preventDefault();
 
-  // Função de movimento de toque
-  function touchMove(event) {
-    event.preventDefault();
-  }
+  // Obter as coordenadas do toque inicial
+  var touch = event.touches[0];
+  startX = touch.clientX - image.offsetLeft;
+  startY = touch.clientY - image.offsetTop;
 
-  // Função de finalização de toque
-  function touchEnd(event) {
-    var touch = event.changedTouches[0];
-    var touchEndX = touch.clientX;
-    var touchEndY = touch.clientY;
+  // Adicionar evento de movimento do toque para atualizar a posição da imagem
+  document.addEventListener('touchmove', moveImage);
+}
 
-    if (
-      touchEndX >= personImage.offsetLeft &&
-      touchEndX <= personImage.offsetLeft + personImage.offsetWidth &&
-      touchEndY >= personImage.offsetTop &&
-      touchEndY <= personImage.offsetTop + personImage.offsetHeight
-    ) {
-      if (isHungry) {
-        personImage.src = 'person-happy.png';
-        statusText.innerHTML = 'A pessoa está alimentada. Obrigado!';
-        foodImage.style.display = 'none';
-        isHungry = false;
-      } else {
-        statusText.innerHTML = 'A pessoa já está alimentada.';
-      }
-    } else {
-      statusText.innerHTML = 'Toque na pessoa para alimentá-la.';
-    }
-  }
+function stopDrag(event) {
+  // Remover evento de movimento do toque
+  document.removeEventListener('touchmove', moveImage);
+}
+
+function moveImage(event) {
+  // Impedir que a página role
+  event.preventDefault();
+
+  // Obter as coordenadas do toque atual
+  var touch = event.touches[0];
+  var currentX = touch.clientX - startX;
+  var currentY = touch.clientY - startY;
+
+  // Atualizar a posição da imagem
+  image.style.left = currentX + 'px';
+  image.style.top = currentY + 'px';
+}
+
 } else {
   var personImage = document.getElementById('person-image');
   var foodImage = document.getElementById('food-image');
