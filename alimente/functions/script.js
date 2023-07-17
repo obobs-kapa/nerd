8edf13f4b51bc8170afdadea8878f03d1f2cf599
obobs <115431830+obobs-kapa@users.noreply.github.com>
2023-07-17 03:40:02 -0300

    Update script.js

--- alimente/functions/script.js
+++ alimente/functions/script.js
@@ -1,56 +1,38 @@
-var personImage = document.getElementById('person-image');
-var foodImage = document.getElementById('food-image');
-var statusText = document.getElementById('status-text');
-var isHungry = true;
+var food = document.getElementById("food-image");
+var person = document.getElementById("person-image");
 
-// Adicionar eventos de toque aos elementos
-personImage.addEventListener('touchstart', touchStart);
-personImage.addEventListener('touchmove', touchMove);
-personImage.addEventListener('touchend', touchEnd);
+food.addEventListener("mousedown", function(e) {
+  e.preventDefault();
+  this.style.cursor = "move";
+  this.style.touchAction = "none";
+});
 
-// Variáveis para rastrear o estado do toque
-var touchStartX;
-var touchStartY;
+food.addEventListener("touchstart", function(e) {
+  e.preventDefault();
+  this.style.cursor = "move";
+  this.style.touchAction = "none";
+});
 
-// Função de início de toque
-function touchStart(event) {
-  var touch = event.touches[0];
-  touchStartX = touch.clientX;
-  touchStartY = touch.clientY;
-}
+food.addEventListener("mousemove", function(e) {
+  this.style.left = e.clientX - this.offsetWidth / 2 + "px";
+  this.style.top = e.clientY - this.offsetHeight / 2 + "px";
+});
 
-// Função de movimento de toque
-function touchMove(event) {
-  event.preventDefault();
-}
+food.addEventListener("touchmove", function(e) {
+  this.style.left = e.touches[0].clientX - this.offsetWidth / 2 + "px";
+  this.style.top = e.touches[0].clientY - this.offsetHeight / 2 + "px";
+});
 
-// Função de finalização de toque
-function touchEnd(event) {
-  var touch = event.changedTouches[0];
-  var touchEndX = touch.clientX;
-  var touchEndY = touch.clientY;
+food.addEventListener("mouseup", function(e) {
+  this.style.cursor = "default";
+});
 
-  var rect = personImage.getBoundingClientRect();
-  var personImageX = rect.left;
-  var personImageY = rect.top;
-  var personImageWidth = rect.width;
-  var personImageHeight = rect.height;
+food.addEventListener("touchend", function(e) {
+  this.style.cursor = "default";
+});
 
-  if (
-    touchEndX >= personImageX &&
-    touchEndX <= personImageX + personImageWidth &&
-    touchEndY >= personImageY &&
-    touchEndY <= personImageY + personImageHeight
-  ) {
-    if (isHungry) {
-      personImage.src = 'erguida.png';
-      statusText.innerHTML = 'A pessoa está alimentada. Obrigado!';
-      foodImage.style.display = 'none';
-      isHungry = false;
-    } else {
-      statusText.innerHTML = 'A pessoa já está alimentada.';
-    }
-  } else {
-    statusText.innerHTML = 'Toque na pessoa para alimentá-la.';
+person.addEventListener("click", function(e) {
+  if (food.contains(e.target)) {
+    alert("Você alimentou a pessoa!");
   }
-}
+});
